4c819fd5ebe4ac8a863bce1698d6543da95a266e
R Widiastuti <r.widiastuti.ft21@mail.umy.ac.id>
2022-11-17 16:01:41 +0700

    add a method to search the list

--- Doubly_Linked_List/Program.cs
+++ Doubly_Linked_List/Program.cs
@@ -72,5 +72,16 @@ namespace Doubly_Linked_List
             current.prev = newnode;
             previous.next = newnode;
         }
+        /*Checks whether the specified node is present*/
+        public bool Search(int rollNo, ref Node previous, ref Node current)
+        {
+            for (previous = current = START; current != null &&
+                rollNo != current.rollNumber; previous = current,
+                current = current.next)
+            { }
+            /*The above for loop traverses the list. If the specified node
+             is found then the function return true, otherwise false.*/
+            return (current != null);
+        }
     }
 }
