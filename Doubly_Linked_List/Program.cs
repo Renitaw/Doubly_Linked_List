66a3eca96e706d7c0db2054f38c6040edba70c00
R Widiastuti <r.widiastuti.ft21@mail.umy.ac.id>
2022-11-17 15:37:51 +0700

    Create a project files

--- Doubly_Linked_List/Program.cs
+++ Doubly_Linked_List/Program.cs
@@ -1,15 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Doubly_Linked_List
 {
-    internal class Program
+    class Node
     {
-        static void Main(string[] args)
-        {
-        }
+       
     }
 }
