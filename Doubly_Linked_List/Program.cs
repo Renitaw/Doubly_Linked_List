0f689a3553f00803e2c9eb7ea7133403e9be2829
R Widiastuti <r.widiastuti.ft21@mail.umy.ac.id>
2022-11-17 15:41:56 +0700

    variable declaration

--- Doubly_Linked_List/Program.cs
+++ Doubly_Linked_List/Program.cs
@@ -4,6 +4,13 @@ namespace Doubly_Linked_List
 {
     class Node
     {
-       
+        /*Node class represents the node of doubly linked list.
+          * It consists of the information part and links to
+          * its succeeding and preceding nodes
+          * in terms of next and previous nodes.*/
+        public int rollNumber;
+        public string name;
+        public Node next;/*points to the succeeding node*/
+        public Node prev;/*points to the preceeding node*/
     }
 }
